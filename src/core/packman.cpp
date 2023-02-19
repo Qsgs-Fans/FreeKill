327c6e9706ca98a99de2832d0ce670673e591381
notify <notify-ctrl@qq.com>
2023-02-19 14:00:59 +0800

    fix git bug

--- src/core/packman.cpp
+++ src/core/packman.cpp
@@ -120,6 +120,11 @@ void PackMan::updatePack(const QString &pack) {
   WHERE name = '%1';").arg(pack));
   if (result.isEmpty()) return;
   int error;
+  error = status(pack);
+  if (error < 0) {
+    qCritical("Workspace is dirty, or some error occured.");
+    return;
+  }
   error = pull(pack);
   if (error < 0) return;
   error = checkout(pack, result[0].toObject()["hash"].toString());
@@ -130,6 +135,11 @@ void PackMan::upgradePack(const QString &pack) {
   int error;
   error = checkout_branch(pack, "master");
   if (error < 0) return;
+  error = status(pack);
+  if (error < 0) {
+    qCritical("Workspace is dirty, or some error occured.");
+    return;
+  }
   error = pull(pack);
   if (error < 0) return;
   ExecSQL(db, QString("UPDATE packages SET hash = '%1' WHERE name = '%2';").arg(head(pack)).arg(pack));
@@ -183,18 +193,20 @@ static int transfer_progress_cb(const git_indexer_progress *stats, void *payload
   return 0;
 }
 
-int PackMan::clone(const QString &url) {
+int PackMan::clone(const QString &u) {
   git_repository *repo = NULL;
-  const char *u = url.toUtf8().constData();
+  auto url = u;
+  while (url.endsWith('/')) {
+    url.chop(1);
+  }
   QString fileName = QUrl(url).fileName();
   if (fileName.endsWith(".git"))
     fileName.chop(4);
   fileName = "packages/" + fileName;
-  const char *path = fileName.toUtf8().constData();
 
   git_clone_options opt = GIT_CLONE_OPTIONS_INIT;
   opt.fetch_opts.callbacks.transfer_progress = transfer_progress_cb;
-  int error = git_clone(&repo, u, path, &opt);
+  int error = git_clone(&repo, url.toUtf8(), fileName.toUtf8(), &opt);
   if (error < 0) {
     GIT_FAIL;
     QDir(fileName).removeRecursively();
@@ -213,20 +225,37 @@ int PackMan::pull(const QString &name) {
   git_repository *repo = NULL;
   int error;
   git_remote *remote = NULL;
-  const char *path = QString("packages/%1").arg(name).toUtf8().constData();
+  auto path = QString("packages/%1").arg(name).toUtf8();
   git_fetch_options opt = GIT_FETCH_OPTIONS_INIT;
   opt.callbacks.transfer_progress = transfer_progress_cb;
+  git_checkout_options opt2 = GIT_CHECKOUT_OPTIONS_INIT;
+  opt2.checkout_strategy = GIT_CHECKOUT_FORCE;
+
   error = git_repository_open(&repo, path);
   if (error < 0) {
     GIT_FAIL;
     goto clean;
   }
+
+  // first git fetch origin
   error = git_remote_lookup(&remote, repo, "origin");
   if (error < 0) {
     GIT_FAIL;
     goto clean;
   }
-  error = git_remote_fetch(remote, NULL, &opt, "pull");
+  error = git_remote_fetch(remote, NULL, &opt, NULL);
+  if (error < 0) {
+    GIT_FAIL;
+    goto clean;
+  }
+
+  // then git checkout FETCH_HEAD
+  error = git_repository_set_head(repo, "FETCH_HEAD");
+  if (error < 0) {
+    GIT_FAIL;
+    goto clean;
+  }
+  error = git_checkout_head(repo, &opt2);
   if (error < 0) {
     GIT_FAIL;
     goto clean;
@@ -245,34 +274,28 @@ clean:
 
 int PackMan::checkout(const QString &name, const QString &hash) {
   git_repository *repo = NULL;
-  git_oid oid = {0};
   int error;
-  git_commit *commit = NULL;
-  const char *path = QString("packages/%1").arg(name).toUtf8().constData();
-  const char *sha = hash.toLatin1().constData();
+  git_checkout_options opt = GIT_CHECKOUT_OPTIONS_INIT;
+  opt.checkout_strategy = GIT_CHECKOUT_FORCE;
+  auto path = QString("packages/%1").arg(name).toUtf8();
+  auto sha = hash.toLatin1();
   error = git_repository_open(&repo, path);
   if (error < 0) {
     GIT_FAIL;
     goto clean;
   }
-  error = git_oid_fromstr(&oid, sha);
+  error = git_repository_set_head(repo, sha);
   if (error < 0) {
     GIT_FAIL;
     goto clean;
   }
-  error = git_commit_lookup(&commit, repo, &oid);
-  if (error < 0) {
-    GIT_FAIL;
-    goto clean;
-  }
-  error = git_checkout_tree(repo, (git_object *)commit, NULL);
+  error = git_checkout_head(repo, &opt);
   if (error < 0) {
     GIT_FAIL;
     goto clean;
   }
 
 clean:
-  git_commit_free(commit);
   git_repository_free(repo);
   return error;
 }
@@ -282,13 +305,13 @@ int PackMan::checkout_branch(const QString &name, const QString &branch) {
   git_oid oid = {0};
   int error;
   git_object *obj = NULL;
-  const char *path = QString("packages/%1").arg(name).toUtf8().constData();
+  auto path = QString("packages/%1").arg(name).toUtf8();
   error = git_repository_open(&repo, path);
   if (error < 0) {
     GIT_FAIL;
     goto clean;
   }
-  error = git_revparse_single(&obj, repo, branch.toUtf8().constData());
+  error = git_revparse_single(&obj, repo, branch.toUtf8());
   if (error < 0) {
     GIT_FAIL;
     goto clean;
@@ -311,7 +334,7 @@ int PackMan::status(const QString &name) {
   git_status_list *status_list;
   size_t i, maxi;
   const git_status_entry *s;
-  const char *path = QString("packages/%1").arg(name).toUtf8().constData();
+  auto path = QString("packages/%1").arg(name).toUtf8();
   error = git_repository_open(&repo, path);
   if (error < 0) {
     GIT_FAIL;
@@ -340,7 +363,7 @@ QString PackMan::head(const QString &name) {
   git_repository *repo = NULL;
   int error;
   git_object *obj = NULL;
-  const char *path = QString("packages/%1").arg(name).toUtf8().constData();
+  auto path = QString("packages/%1").arg(name).toUtf8();
   error = git_repository_open(&repo, path);
   if (error < 0) {
     GIT_FAIL;
