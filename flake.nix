4fdbe6b10f62cabe3e42ed95c0af071ad4235234
xinyangli <lixinyang411@gmail.com>
2023-04-08 03:00:36 +0800

    feat: use nix to build

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Sanguosha (a.k.a. Legend of Three Kingdoms, LTK) written in Qt and Lua.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+  };
+
+  outputs = { self, nixpkgs }: {
+    defaultPackage.x86_64-linux =
+      with import nixpkgs { system = "x86_64-linux"; };
+      stdenv.mkDerivation {
+        name = "freekill";
+        version = "0.0.7";
+        src = self;
+
+        buildInputs = with qt6; [
+          qtbase
+          qtdeclarative
+          qt5compat
+          qtmultimedia
+          qttools
+          sqlite
+          swig
+          openssl
+          flex
+          bison
+          readline
+          libgit2
+          lua5_4
+        ];
+        nativeBuildInputs = [ cmake qt6.wrapQtAppsHook ];
+
+        meta = with lib; { };
+      };
+  };
+}
