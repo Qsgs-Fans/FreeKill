e71aab37fc38b73af595c7956eb6cfd819636fb5
Mechanel <nyutanislavsky@qq.com>
2023-05-31 15:56:48 +0800

    add excludeFrom for MaxCardsSkill
    Signed-off-by: Mechanel <nyutanislavsky@qq.com>

--- lua/core/skill_type/max_cards.lua
+++ lua/core/skill_type/max_cards.lua
@@ -4,17 +4,22 @@
 local MaxCardsSkill = StatusSkill:subclass("MaxCardsSkill")
 
 ---@param from Player
----@param to Player
 ---@return integer
 function MaxCardsSkill:getFixed(player)
   return nil
 end
 
 ---@param from Player
----@param to Player
 ---@return integer
 function MaxCardsSkill:getCorrect(player)
   return 0
+end
+
+---@param from Player
+---@param card Card
+---@return boolean
+function MaxCardsSkill:excludeFrom(player, card)
+  return false
 end
 
 return MaxCardsSkill
