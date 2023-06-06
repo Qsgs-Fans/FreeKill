3f3615f5b03487573990d76426f639d595d4aa29
scretxc <134952525+scretxc@users.noreply.github.com>
2023-06-06 15:56:22 +0800

    Update client.lua

--- lua/client/client.lua
+++ lua/client/client.lua
@@ -23,7 +23,7 @@ function Client:initialize()
   self.client.callback = function(_self, command, jsonData)
     local cb = fk.client_callback[command]
 
-    if command ~= "Heartbeat" and command ~= "changeSelf" then
+    if command ~= "Heartbeat" and command ~= "StartChangeSelf" and command ~= "ChangeSelf" then
       Fk.currentResponsePattern = nil
       Fk.currentResponseReason = nil
     end
