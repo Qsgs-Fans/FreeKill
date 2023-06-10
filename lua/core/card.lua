939ea4df732ec77e6282e98824421a6625cf1991
YoumuKon <38815081+YoumuKon@users.noreply.github.com>
2023-06-11 01:19:59 +0800

    setMark的0判断 x2

--- lua/core/card.lua
+++ lua/core/card.lua
@@ -315,6 +315,7 @@ end
 ---@param mark string @ 标记
 ---@param count integer @ 为标记删除的数量
 function Card:setMark(mark, count)
+  if count == 0 then count = nil end
   if self.mark[mark] ~= count then
     self.mark[mark] = count
   end
