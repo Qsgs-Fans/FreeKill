d4ec22f5f6a31b73bf49a48ebfaa504b430a1f06
notify <notify-ctrl@qq.com>
2023-06-23 15:14:15 +0800

    pattern test

--- test/lua/pattern.lua
+++ test/lua/pattern.lua
@@ -1,5 +1,22 @@
+local exp1 = Exppattern:Parse("slash,jink")
+local exp2 = Exppattern:Parse("peach,jink")
+local exp3 = Exppattern:Parse(".|.|.|.|.|trick")
+local exp4 = Exppattern:Parse("peach,ex_nihilo")
+
+local slash = Fk:cloneCard("slash")
+
 TestExppattern = {
-  testParse = function()
-    lu.assertEquals(1, 2)
+  testMatchExp = function()
+    assert(exp1:matchExp(exp2))
+  end,
+
+  testEasyMatchCard = function()
+    assert(exp1:match(slash))
+    assert(not exp2:match(slash))
+  end,
+
+  testMatchWithType = function()
+    assert(not exp3:matchExp(exp1))
+    assert(exp3:matchExp(exp4))
   end,
 }
