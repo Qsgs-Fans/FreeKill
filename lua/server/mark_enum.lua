34030c2fe9d79c9bb22dc698157ebcadaf7a7f8b
YoumuKon <38815081+YoumuKon@users.noreply.github.com>
2023-07-08 22:49:53 +0800

    重量级之——清理后缀

--- lua/server/mark_enum.lua
+++ lua/server/mark_enum.lua
@@ -28,7 +28,14 @@ MarkEnum.BypassTimesLimitTo = "BypassTimesLimitTo"
 ---@field BypassDistancesLimitTo string @ 对其使用牌无距离限制，可带清除标记后缀
 MarkEnum.BypassDistancesLimitTo = "BypassDistancesLimitTo"
 ---@field UncompulsoryInvalidity string @ 非锁定技失效，可带清除标记后缀
-MarkEnum.UncompulsoryInvalidity = "uncompulsoryInvalidity"
+MarkEnum.UncompulsoryInvalidity = "UncompulsoryInvalidity"
 
 ---@field TempMarkSuffix string[] @ 各种清除标记后缀
 MarkEnum.TempMarkSuffix = { "-phase", "-turn", "-round" }
+
+---@field NotIncludeMaxCards string @ 不计入手牌上限，可带清除标记后缀
+MarkEnum.NotIncludeMaxCards = "NotIncludeMaxCards"
+---@field NoDiscard string @ 不可弃置，可带清除标记后缀
+MarkEnum.NoDiscard = "NoDiscard"
+---@field CardTempMarkSuffix string[] @ 卡牌标记版本的清除标记后缀
+MarkEnum.CardTempMarkSuffix = { "-phase", "-turn", "-round", "-inhand" } 
