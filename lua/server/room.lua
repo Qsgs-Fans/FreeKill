f4e650aec4661da3ea6ad82f5cd5baa1aec919f9
Mechanel <nyutanislavsky@qq.com>
2023-07-16 02:48:51 +0800

    add return for askForMoveCardInBoard
    Signed-off-by: Mechanel <nyutanislavsky@qq.com>

--- lua/server/room.lua
+++ lua/server/room.lua
@@ -1788,7 +1788,7 @@ end
 ---@param skillName string @ 技能名
 ---@param flag string|null @ 限定可移动的区域，值为nil（装备区和判定区）、‘e’或‘j’
 ---@param moveFrom ServerPlayer|null @ 是否只是目标1移动给目标2
----@return integer
+---@return table<"card"|"from"|"to"> @ 选择的卡牌id、起点玩家id和终点玩家id列表
 function Room:askForMoveCardInBoard(player, targetOne, targetTwo, skillName, flag, moveFrom)
   if flag then
     assert(flag == "e" or flag == "j")
@@ -1866,15 +1866,18 @@ function Room:askForMoveCardInBoard(player, targetOne, targetTwo, skillName, fla
   end
 
   local cardToMove = Fk:getCardById(result.cardId)
+  local from, to = result.pos == 0 and targetOne, targetTwo or targetTwo, targetOne
   self:moveCardTo(
     cardToMove,
     cardToMove.type == Card.TypeEquip and Player.Equip or Player.Judge,
-    result.pos == 0 and targetTwo or targetOne,
+    to,
     fk.ReasonPut,
     skillName,
     nil,
     true
   )
+
+  return { card = result.cardId, from = from.id, to = to.id }
 end
 
 --- 询问一名玩家从targets中选择出若干名玩家来移动场上的牌。
