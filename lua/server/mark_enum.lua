a29b2e35a706b06ee8ae2cde88b17e5d500153f1
YoumuKon <38815081+YoumuKon@users.noreply.github.com>
2023-08-02 03:29:29 +0800

    bug fix

--- lua/server/mark_enum.lua
+++ lua/server/mark_enum.lua
@@ -2,21 +2,21 @@
 
 MarkEnum = {}
 
--- 跳过觉醒标记(值为技能名通过+连接)
+---跳过觉醒标记(值为技能名通过+连接)
 MarkEnum.StraightToWake = "_straight_to_wake"
 
---转换技状态标记前缀（整体为前缀+转换技技能）
+---转换技状态标记前缀（整体为前缀+转换技技能）
 MarkEnum.SwithSkillPreName = "__switcher_"
---使命技状态标记前缀（整体为前缀+使命技技能）
+---使命技状态标记前缀（整体为前缀+使命技技能）
 MarkEnum.QuestSkillPreName = "__questPre_"
 
---增加标记值数量的手牌上限
+---增加标记值数量的手牌上限
 MarkEnum.AddMaxCards = "AddMaxCards"
---于本回合内增加标记值数量的手牌上限
+---于本回合内增加标记值数量的手牌上限
 MarkEnum.AddMaxCardsInTurn = "AddMaxCards-turn"
---减少标记值数量的手牌上限
+---减少标记值数量的手牌上限
 MarkEnum.MinusMaxCards = "MinusMaxCards"
---于本回合内减少标记值数量的手牌上限
+---于本回合内减少标记值数量的手牌上限
 MarkEnum.MinusMaxCardsInTurn = "MinusMaxCards-turn"
 
 ---使用牌无次数限制，可带清除标记后缀（-tmp为请求专用）
