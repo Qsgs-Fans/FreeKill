17390209ef7449da3a83574ab8473bd8ab7f7019
YoumuKon <38815081+YoumuKon@users.noreply.github.com>
2023-10-30 16:04:02 +0800

    ex__choose_skill

--- lua/server/room.lua
+++ lua/server/room.lua
@@ -1052,7 +1052,7 @@ end
 ---@param cancelable bool @ 是否可以点取消
 ---@param extra_data table|nil @ 额外信息，因技能而异了
 ---@param no_indicate bool @ 是否不显示指示线
----@return boolean, table
+---@return boolean, table|nil
 function Room:askForUseActiveSkill(player, skill_name, prompt, cancelable, extra_data, no_indicate)
   prompt = prompt or ""
   cancelable = (cancelable == nil) and true or cancelable
@@ -1318,6 +1318,55 @@ function Room:askForChooseCardAndPlayers(player, targets, minNum, maxNum, patter
       return {}
     else
       return table.random(targets, minNum), table.random(pcards)
+    end
+  end
+end
+
+--- 询问玩家选择X张牌和Y名角色。
+---
+--- 返回两个值，第一个是选择的目标列表，第二个是选择的那张牌的id
+---@param player ServerPlayer @ 要询问的玩家
+---@param minCardNum integer @ 选卡牌最小值
+---@param maxCardNum integer @ 选卡牌最大值
+---@param targets integer[] @ 选择目标的id范围
+---@param minTargetNum integer @ 选目标最小值
+---@param maxTargetNum integer @ 选目标最大值
+---@param pattern string|nil @ 选牌规则
+---@param prompt string|nil @ 提示信息
+---@param cancelable bool @ 能否点取消
+---@param no_indicate bool @ 是否不显示指示线
+---@return integer[], integer[]
+function Room:askForChooseBoth(player, minCardNum, maxCardNum, targets, minTargetNum, maxTargetNum, pattern, prompt, skillName, cancelable, no_indicate)
+  if minCardNum < 1 or minTargetNum < 1 then
+    return table.unpack({}, {})
+  end
+  cancelable = (cancelable == nil) and true or cancelable
+  no_indicate = no_indicate or false
+  pattern = pattern or "."
+
+  local pcards = table.filter(player:getCardIds({ Player.Hand, Player.Equip }), function(id)
+    local c = Fk:getCardById(id)
+    return c:matchPattern(pattern)
+  end)
+  if #pcards < minCardNum and not cancelable then return table.unpack({}, {}) end
+
+  local data = {
+    targets = targets,
+    max_target_num = maxTargetNum,
+    min_target_num = minTargetNum,
+    max_card_num = maxCardNum,
+    min_card_num = minCardNum,
+    pattern = pattern,
+    skillName = skillName,
+  }
+  local _, ret = self:askForUseActiveSkill(player, "ex__choose_skill", prompt or "", cancelable, data, no_indicate)
+  if ret then
+    return ret.targets, ret.cards
+  else
+    if cancelable then
+      return table.unpack({}, {})
+    else
+      return table.random(targets, minTargetNum), table.random(pcards, minCardNum)
     end
   end
 end
