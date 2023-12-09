0370c089af28e9e610060bbff163dbf8750d666e
notify <notify-ctrl@qq.com>
2023-12-09 23:04:55 +0800

    勉强算修了randomAI

--- lua/server/ai/random_ai.lua
+++ lua/server/ai/random_ai.lua
@@ -15,17 +15,18 @@ function RandomAI:useActiveSkill(skill, card)
     filter_func = Util.FalseFunc
   end
 
-  if self.command == "PlayCard" and card and (not skill:canUse(player, card) or player:prohibitUse(card)) then
+  if self.command == "PlayCard" and (not skill:canUse(player, card) or player:prohibitUse(card)) then
     return ""
   end
 
   local max_try_times = 100
   local selected_targets = {}
   local selected_cards = {}
-  local min = skill:getMinTargetNum()
-  local max = skill:getMaxTargetNum(player, card)
-  local min_card = skill:getMinCardNum()
-  local max_card = skill:getMaxCardNum()
+  -- FIXME: ...
+  -- local min = skill:getMinTargetNum()
+  -- local max = skill:getMaxTargetNum(player, card)
+  -- local min_card = skill:getMinCardNum()
+  -- local max_card = skill:getMaxCardNum()
   for _ = 0, max_try_times do
     if skill:feasible(selected_targets, selected_cards, self.player, card) then break end
     local avail_targets = table.filter(room:getAlivePlayers(), function(p)
