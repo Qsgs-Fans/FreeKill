9165d70eed47bb8d6a71521df27a5533593b20ab
YoumuKon <38815081+YoumuKon@users.noreply.github.com>
2024-01-16 21:27:33 +0800

    fix

--- lua/core/player.lua
+++ lua/core/player.lua
@@ -363,13 +363,13 @@ function Player:getCardIds(playerAreas, specialName)
   return cardIds
 end
 
---- 通过名字检索获取玩家是否存在对应私人牌堆。
+--- 通过名字检索获取玩家对应的私人牌堆。
 ---@param name string @ 私人牌堆名
 function Player:getPile(name)
   return table.simpleClone(self.special_cards[name]) or {}
 end
 
---- 通过ID检索获取玩家是否存在对应私人牌堆。
+--- 通过ID检索获取玩家对应的私人牌堆。
 ---@param id integer @ 私人牌堆ID
 ---@return string?
 function Player:getPileNameOfId(id)
