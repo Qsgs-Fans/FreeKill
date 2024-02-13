113fb9e00a7c139451f3b8f0ccc0774e1e7b3120
notify <notify-ctrl@qq.com>
2024-02-13 19:20:00 +0800

    kill hasMark

--- lua/core/card.lua
+++ lua/core/card.lua
@@ -414,14 +414,6 @@ function Card:getMark(mark)
   return ret
 end
 
---- 判定卡牌是否拥有对应的Mark。
----@param mark string @ 标记
----@return boolean
-function Card:hasMark(mark)
-  fk.qWarning("hasMark will be deleted in future version!")
-  return self:getMark(mark) ~= 0
-end
-
 --- 获取卡牌有哪些Mark。
 function Card:getMarkNames()
   local ret = {}
