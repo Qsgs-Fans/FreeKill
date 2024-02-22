2e7f90ec6a71464ac254d8f9ff6a8ffbd3937a1e
YoumuKon <38815081+YoumuKon@users.noreply.github.com>
2024-02-22 22:20:05 +0800

    maxcard

--- packages/standard/aux_skills.lua
+++ packages/standard/aux_skills.lua
@@ -129,11 +129,25 @@ local maxCardsSkill = fk.CreateMaxCardsSkill{
   name = "max_cards_skill",
   global = true,
   correct_func = function(self, player)
+    local function getMark(markname)
+      local v = 0
+      for mark, value in pairs(player.mark) do
+        if mark == markname then
+          v = v + value
+        elseif mark:startsWith(markname .. "-") then
+          for _, suffix in ipairs(MarkEnum.TempMarkSuffix) do
+            if mark:find(suffix, 1, true) then
+              v = v + value
+              break
+            end
+          end
+        end
+      end
+      return v
+    end
     return
-      player:getMark(MarkEnum.AddMaxCards) +
-      player:getMark(MarkEnum.AddMaxCardsInTurn) -
-      player:getMark(MarkEnum.MinusMaxCards) -
-      player:getMark(MarkEnum.MinusMaxCardsInTurn)
+      getMark(MarkEnum.AddMaxCards) -
+      getMark(MarkEnum.MinusMaxCards)
   end,
 }
 
