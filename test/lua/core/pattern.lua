e33984951c2fc23860204e70f3ac8d4ccf9cc0d5
YoumuKon <38815081+YoumuKon@users.noreply.github.com>
2024-12-03 01:04:58 +0800

    testAdvanceMatchCard

--- test/lua/core/pattern.lua
+++ test/lua/core/pattern.lua
@@ -15,6 +15,24 @@ function TestExppattern:testEasyMatchCard()
   lu.assertFalse(exp2:match(slash))
 end
 
+function TestExppattern:testAdvanceMatchCard()
+  local exp1 = Exppattern:Parse("slash,jink")
+  local exp2 = Exppattern:Parse(".|.|.|.|peach,slash")
+  local slash = Fk:cloneCard("slash")
+  lu.assertTrue(exp1:match(slash))
+  lu.assertTrue(exp2:match(slash))
+  local t_slash = Fk:cloneCard("thunder__slash")
+  lu.assertTrue(exp1:match(t_slash))
+  lu.assertFalse(exp2:match(t_slash))
+
+  lu.assertTrue(exp1:matchExp(exp2))
+  lu.assertTrue(exp2:matchExp(exp1))
+  local exp3 = Exppattern:Parse(".|.|.|.|thunder__slash")
+  lu.assertTrue(exp1:matchExp(exp3))
+  lu.assertFalse(exp3:matchExp(exp2))
+  lu.assertTrue(exp3:matchExp(exp1))
+end
+
 function TestExppattern:testMatchWithType()
   local exp3 = Exppattern:Parse(".|.|.|.|.|normal_trick")
   lu.assertFalse(exp3:matchExp("slash,jink"))
